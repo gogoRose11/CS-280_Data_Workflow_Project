927490dc4ed98dc6b1448c284e69aa4fc40c29b1
ericrose <ericrose@Eric-Rose-CS280.myguest.virtualbox.org>
2023-02-22 13:25:57 -0700

    testing users_list

--- twitter_dag.py
+++ twitter_dag.py
@@ -3,9 +3,37 @@ import logging as log
 import pendulum
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
+from models.config import Session #You would import this from your config file
+from models.users import User
+
+session = Session()
+
+# This will retrieve all of the users from the database 
+# (It'll be a list, so you may have 100 users or 0 users)
+session.query(User).all() 
+
+# This will retrieve the user who's username is NASA
+nasaUser = session.query(User).filter(User.username == "NASA").first()
+
+#You can then print the username of the user you retrieved
+print(nasaUser.username)
+
+#We recommend that you reassign the user to a variable so that you can use it later
+nasaUsername = nasaUser.username
+
+# This will close the session that you opened at the beginning of the file.
+session.close()
 
 def load_data_task_function():
     log.info("ENTERED: LOAD DATA TASK FUNCTION")
+
+    session = Session()
+    # This will retrieve all of the users from the database 
+    # (It'll be a list, so you may have 100 users or 0 users)
+    users_list = session.query(User).all() 
+    log.info(f"USER LIST: {users_list}")
+
+    # PULL USER INFORMATION AND PASS IT TO THE NEXT TASK
     
     return
 
