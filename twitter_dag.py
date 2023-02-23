148e12747359ffdc3217af501a8eeb7d86d8b076
ericrose <ericrose@Eric-Rose-CS280.myguest.virtualbox.org>
2023-02-22 17:16:19 -0700

    printing pds dataframe

--- twitter_dag.py
+++ twitter_dag.py
@@ -8,6 +8,7 @@ from models.config import Session #You would import this from your config file
 from models.user import User
 from airflow.models import Variable
 from airflow.models import TaskInstance
+import pandas as pd
 
 
 def get_auth_header():
@@ -37,9 +38,10 @@ def load_data_task_function(ti: TaskInstance, **kwargs):
     # last 5 tweets
     tweets_url = f'https://api.twitter.com/1.1/statuses/user_timeline.json'
     tweets_list = []
+    tweet_fields = "public_metrics,author_id,text,created_at"
     for user in users_list:
         log.info(f"Getting TWEETS for: {user.name}")
-        response = requests.get(tweets_url, headers=get_auth_header(), params={"user_id": user.user_id, "count": 5})
+        response = requests.get(tweets_url, headers=get_auth_header(), params={"user_id": user.user_id, "tweet.fields": tweet_fields, "count": 5})
         tweets_list.append(response.json())
     
     log.info(f"TWEETS LIST: {tweets_list}")
@@ -61,7 +63,7 @@ def call_api_task_function(ti: TaskInstance, **kwargs):
     # GET UPDATED STATISTICS FOR EVERY USER
     user_requests = []
     #user_ids = Variable.get(f"TWITTER_USER_IDS", [], deserialize_json=True)
-    user_fields = "public_metrics,username,id"
+    user_fields = "public_metrics,username,id,name,created_at"
     for user in users_list:
         user_url = f"https://api.twitter.com/2/users/{user.user_id}"
         request = requests.get(user_url, headers=get_auth_header(), params={"user.fields": user_fields})
@@ -77,8 +79,51 @@ def call_api_task_function(ti: TaskInstance, **kwargs):
 
 def transform_data_task_function(ti: TaskInstance, **kwargs):
     log.info("ENTERED: TRANSFORM DATA TASK FUNCTION")
-    
+    users_list = ti.xcom_pull(key='users_list', task_ids='call_api_task')
+    tweets_requests = ti.xcom_pull(key='tweet_list', task_ids='call_api_task')
+    user_requests = ti.xcom_pull(key='users_requests', task_ids='call_api_task')
+
+    user_df = get_user_pd(user_requests)
     return
+
+
+def get_tweet_pd(tweet_requests):
+    tweet_df = pd.DataFrame(columns=['id','text','retweet_count','reply_count','like_count','quote_count','impression_count'])
+
+    # LOOP THROUGH TWEET RESPONSES
+    for tweet in tweet_requests:
+        #resp = tweet.json()
+        resp = tweet
+        data = resp.get('data')
+        del data['edit_history_tweet_ids']
+        del data['author_id']
+        pub = data['public_metrics']
+        del data['public_metrics']
+        data.update(pub)
+        tweet_df = tweet_df.append(data, ignore_index=True)
+
+    return tweet_df
+
+
+def get_user_pd(user_requests):
+    user_df = pd.DataFrame(columns=['id','username','name','followers_count','following_count','tweet_count','listed_count','created_at'])
+
+    # LOOP THROUGH USER RESPONSES
+    for user in user_requests:
+        #log.info("ENTERED USER REQUESTS FOR LOOP - GET USER PD")
+        #resp = user.json()
+        resp = user
+        data = resp.get('data')
+        #del data['profile_image_url']
+        #del data['description']
+        pub = data['public_metrics']
+        del data['public_metrics']
+        data.update(pub)
+        user_df = user_df.append(data, ignore_index=True)
+
+    log.info("USER DATAFRAME AT THE END OF GET USER PD")
+    log.info(user_df)
+    return user_df
 
 def write_data_task_function(ti: TaskInstance, **kwargs):
     log.info("ENTERED: WRITE DATA TASK FUNCTION")
