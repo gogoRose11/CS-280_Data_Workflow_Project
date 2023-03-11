7bdeff13b42b76c57f138bc9016349370cea9afb
Eric Rose <ericsandersrose@gmail.com>
2023-03-11 08:26:05 -0700

    slug

--- midterm.py
+++ midterm.py
@@ -27,7 +27,7 @@ def populate_countries_table(country_list):
 
 def pop_country_totals(country_list):
 
-    country_name = country_list[0]['Country']
+    country_name = country_list[0]['Slug']
 
     x = requests.get(f"https://api.covid19api.com/country/{country_name}?from=2020-03-01T00:00:00Z&to=2022-03-01T00:00:00Z")
     print(f"COUNTRY TOTALS FOR: {country_name}")
