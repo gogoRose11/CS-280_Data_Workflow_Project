2d9ab793ea544592686155169e898513ec074b86
Eric Rose <ericsandersrose@gmail.com>
2023-03-11 10:52:03 -0700

    fixed query

--- midterm.py
+++ midterm.py
@@ -41,7 +41,7 @@ def pop_country_totals(country_list):
     print(totals[0])
 
     session = Session()
-    real_country_id = session.query.(Country).filter(Country.slug == country_name).first()
+    real_country_id = session.query(Country).filter(Country.slug == country_name).first()
 
     for i in range(len(totals)):
 
